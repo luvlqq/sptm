65885e39e31c713dcb89cb32c1bf6ad3a763f6bb
luvlqq <lualgod92@gmail.com>
2023-03-22 18:32:57 +0300

    remove mongo from app.module

--- books/src/app.module.ts
+++ books/src/app.module.ts
@@ -3,21 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    AuthModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:adminadmin@cluster0.z1jmopj.mongodb.net/?retryWrites=true&w=majority',
-    ),
-    BooksModule,
-    PrismaModule,
-  ],
+  imports: [UsersModule, AuthModule, BooksModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
